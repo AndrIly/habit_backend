a85f0f4784c1535dbd56edfa3c69d5da32273bed
Reviva1w <ilaandrianov430@gmail.com>
2026-02-08 15:40:41 +0300

    new update 0.2

--- config_data/config.py
+++ config_data/config.py
@@ -1,5 +1,10 @@
 import os
 
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except Exception:
+    pass
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 RAPID_API_KEY = os.getenv("RAPID_API_KEY")
 JWT_SECRET = os.getenv("JWT_SECRET")
