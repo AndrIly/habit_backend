d7e1dbf2673bedc837f8739d59ce9198dac7f20e
Reviva1w <ilaandrianov430@gmail.com>
2026-02-09 21:17:25 +0300

    new update 0.4

--- handlers/custom_handlers/webapp.py
+++ handlers/custom_handlers/webapp.py
@@ -1,8 +1,27 @@
 import json
 from telebot.types import Message
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from database.tokens import upsert_token
 from keyboards.reply.button_reply import main_menu
+from keyboards.reply.webapp_reply import WEBAPP_URL
 from loader import bot
+
+
+@bot.message_handler(func=lambda m: m.text == "🔐 Войти")
+def open_webapp_via_inline(message: Message):
+    kb = InlineKeyboardMarkup()
+    kb.add(
+        InlineKeyboardButton(
+            text="Открыть авторизацию",
+            web_app=WebAppInfo(url=WEBAPP_URL)
+        )
+    )
+    bot.send_message(
+        message.chat.id,
+        "Нажми кнопку ниже, чтобы открыть авторизацию внутри Telegram.",
+        reply_markup=kb
+    )
+
 
 @bot.message_handler(content_types='web_app_data')
 def on_webapp_data(message: Message):
@@ -16,4 +35,3 @@ def on_webapp_data(message: Message):
 
     upsert_token(message.from_user.id, token)
     bot.send_message(message.chat.id, '✅Авторизация прошла успешно✅', reply_markup=main_menu())
-
--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,18 +1,11 @@
-from telebot import types
 from telebot.types import Message
 from loader import bot
-
-WEBAPP_URL = "https://habit-backend-awul.onrender.com/webapp"
+from keyboards.reply.webapp_reply import auth_reply
 
 @bot.message_handler(commands=["start"])
 def start(message: Message):
-    bot.set_chat_menu_button(
-        chat_id=message.chat.id,
-        menu_button=types.MenuButtonWebApp(
-            type = 'web_app',
-            text="Открыть приложение",
-            web_app=types.WebAppInfo(url=WEBAPP_URL)
-        )
+    bot.send_message(
+        message.chat.id,
+        "Нажми кнопку «🔐 Войти» ниже для авторизации.",
+        reply_markup=auth_reply()
     )
-    bot.send_message(message.chat.id, "Открой мини-приложение через кнопку меню «Открыть приложение».")
-
